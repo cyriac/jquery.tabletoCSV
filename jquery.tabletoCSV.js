6e18c11fe6140187b33bb8022ee9d57824b9077c
Cyriac Thomas <cyriac@compile.com>
2013-07-28 11:09:58 +0530

    initial commit

---
+++ jquery.tabletoCSV.js
@@ -0,0 +1,36 @@
+jQuery.fn.tableToCSV = function() {
+	$(this).each(function(){
+			var table = $(this);
+			var caption = $(this).find('caption').text();
+			var title = [];
+			var rows = [];
+
+			$(this).find('tr').each(function(){
+				var data = [];
+				$(this).find('th').each(function(){
+					title.push($(this).text());
+					});
+				$(this).find('td').each(function(){
+					data.push($(this).text());
+					});
+				data = data.join(",");
+				rows.push(data);
+				});
+			title = title.join(",");
+			rows = rows.join("\n");
+
+			var csv = title + rows;
+			var uri = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv);
+			var download_link = document.createElement('a');
+			download_link.href = uri;
+			var ts = new Date().getTime();
+			if(caption==""){
+				download_link.download = ts+".csv";
+			} else {
+				download_link.download = caption+"-"+ts+".csv";
+			}
+			document.body.appendChild(download_link);
+			download_link.click();
+			document.body.removeChild(download_link);
+	});
+};
